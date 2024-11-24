7fd2dfec66a3ea7992021ec7b51ce36e3700d0ad
Mohitpatil344 <mr.mohitpatil003@gmail.com>
2024-11-24 19:39:39 +0530

    header fixed

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,23 @@
 import { Outlet } from "react-router-dom";
-import React from 'react'
-import Header from "./Header";
-import Footer from "./Footer ";
+import React from 'react';
+import Header from "../components/Header";
+import Footer from "../components/Footer ";
 
 const Layout = () => {
   return (
-    <>
-        <Header/>
-        <Outlet/>
-        <Footer/>
-    </>
-  )
-}
+    <div className="relative">
+      {/* Fixed Header */}
+      <Header className="fixed top-0 left-0 right-0 z-50 w-full" />
 
-export default Layout
+      {/* Content Wrapper */}
+      <div className="mt-[var(--header-height)]"> {/* Adjust padding based on header height */}
+        <Outlet />
+      </div>
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
